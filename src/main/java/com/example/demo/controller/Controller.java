8a2d76f0e8741a8fabfc87d4d00e9140dff14241
Philip Omayuku <pomayuku@yahoo.com>
2024-09-05 07:35:30 +0100

    Tweak made

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import java.util.List;
 @RestController
 public class Controller {
 
-    @PostMapping("/calculate")
+    @GetMapping("/calculate")
     public int calculateSum(@RequestBody List<Integer> numbers) {
         return numbers.stream().mapToInt(Integer::intValue).sum();
     }
